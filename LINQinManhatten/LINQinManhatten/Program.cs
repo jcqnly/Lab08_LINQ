8498ace8510146901fbd22bd5175984eed01343e
jcqnly <jcqnly@gmail.com>
2018-06-23 23:58:54 -0700

    add query to filter out null neighborhood entries

--- LINQinManhatten/LINQinManhatten/Program.cs
+++ LINQinManhatten/LINQinManhatten/Program.cs
@@ -5,35 +5,68 @@ using System.Linq;
 
 namespace LINQinManhatten
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             LoadJSON();
         }
 
         /// <summary>
-        /// Read the entire JSON file into an object, pass that object
-        /// to another method to start going through the data
+        /// Read the entire JSON file into an object that is
+        /// instantiated from the RootObject class, pass that object
+        /// to another method to start parsing the data
         /// </summary>
         public static void LoadJSON()
         {
+            //There were build errors, so the data file is on the same
+            //level as the gitignore file
             string path = @"../../../../../data.json";
             //read the entire json file into an instantiation of the RootObject called FeatureCollection
+            //use Sysem.IO to ReadAllText from that file
             RootObject FeatureCollection = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(
                 File.ReadAllText(path));
 
             ShowNeigborhoods(FeatureCollection);
         }
 
+        /// <summary>
+        /// parse through the data for only the neighborhoods
+        /// output all the neighborhoods
+        /// </summary>
+        /// <param name="FeatureCollection"></param>
         public static void ShowNeigborhoods(RootObject FeatureCollection)
         {
+            //LINQ query
             var neighborhoods = from i in FeatureCollection.Features
                                 select i.Properties.Neighborhood;
             foreach (var hoods in neighborhoods)
             {
                 Console.WriteLine(hoods);
             }
+            Console.Read();
+            Console.Clear();
+            FilterNoNameNeighborhoods(FeatureCollection);
+        }
+
+        /// <summary>
+        /// Filter out entries that have no name for the neighborhood
+        /// </summary>
+        /// <param name="FeatureCollection"></param>
+        public static void FilterNoNameNeighborhoods(RootObject FeatureCollection)
+        {
+            var neighborhoods = from x in FeatureCollection.Features
+                                where x.Properties.Neighborhood != ""
+                                select x.Properties.Neighborhood;
+            foreach (var hoods in neighborhoods)
+            {
+                Console.WriteLine(hoods);
+            }
+        }
+
+        public static void FilterOutDupes(RootObject FeatureCollection)
+        {
+
         }
     }
 }
